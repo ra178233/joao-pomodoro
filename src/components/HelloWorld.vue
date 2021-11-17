5790e942de589bb850cb920e4c3868817b23d05d
unknown <RA178233@UCDB.BR>
2021-11-17 09:28:08 -0400

    make it better

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,120 +1,132 @@
 <template>
+<div>
   <div class="hello">
     <h1>{{ msg }}</h1>
   </div>
-   <div class="container">
-      <h2 v-if="pomodoroMode">Modo de trabalho</h2>
-      <h2 v-if="!pomodoroMode">Modo de intervalo</h2>
+  <div class="container">
+    <h2 v-if="pomodoroMode">Modo de trabalho</h2>
+    <h2 v-if="!pomodoroMode">Modo de intervalo</h2>
   </div>
   <div class="container">
-    <div class="text-center">
-      {{ minutos }}:{{ segundos }}
-    </div>
+    <div class="text-center">{{ minutos }}:{{ segundos }}</div>
   </div>
   <div class="container">
-      <button v-on:click="start"><i class="fas fa-play"></i> start</button>
-      <button v-on:click="stop"><i class="fas fa-stop"></i> stop</button>
-  </div>
-   <div class="container">
-      <button v-if="!working" v-on:click="diminuirTempo"><i class="fas fa-minus"></i></button>
-      <button v-if="!working" v-on:click="alterarMode"> mode </button>
-      <button v-if="!working" v-on:click="aumentarTempo"><i class="fas fa-plus"></i></button>
+    <button v-on:click="start"><i class="fas fa-play"></i> start</button>
+    <button v-on:click="stop"><i class="fas fa-stop"></i> stop</button>
   </div>
   <div class="container">
-      <h1>Contador de pomodoros: {{contador}}</h1>
+    <button v-if="!working" v-on:click="diminuirTempo">
+      <i class="fas fa-minus"></i>
+    </button>
+    <button v-if="!working" v-on:click="alterarMode">mode</button>
+    <button v-if="!working" v-on:click="aumentarTempo">
+      <i class="fas fa-plus"></i>
+    </button>
   </div>
+  <div class="container">
+    <h1>Contador de pomodoros: {{ contador }}</h1>
+  </div>
+</div>
 </template>
 
 <script>
-import alarm from '@/assets/alarm.mp3'
+import alarm from "@/assets/alarm.mp3";
 export default {
-  name: 'HelloWorld',
+  name: "HelloWorld",
   props: {
-    msg: String
+    msg: String,
   },
-  data(){
-    return{
+  data() {
+    return {
       minutos: 25,
-      segundos: '00',
+      segundos: "00",
       working: false,
       pomodoroMode: true,
-      contador: 0
-    }
+      contador: 0,
+    };
   },
   methods: {
     start() {
-      this.working = true
-      if(this.finalizado()){
-        this.segundos = '00'
-        this.minutos = this.pomodoroMode ? 25 : 5
-        return
+      this.working = true;
+      if (this.finalizado()) {
+        this.segundos = "00";
+        this.minutos = this.pomodoroMode ? 25 : 5;
+        return;
       }
-      if(this.iniciado()){
-         let segundo = Number(this.segundos)
-         segundo <= 10 ? this.segundos = `0${this.segundos - 1}` : this.segundos = `${this.segundos - 1}`;
-         return
+      if (this.iniciado()) {
+        let segundo = Number(this.segundos);
+        segundo <= 10
+          ? (this.segundos = `0${this.segundos - 1}`)
+          : (this.segundos = `${this.segundos - 1}`);
+        return;
       }
-      this.segundos = '59'
+      this.segundos = "59";
       this.minutos--;
     },
     stop() {
-      this.working = false
+      this.working = false;
     },
-    alterarMode(){
-      this.pomodoroMode = !this.pomodoroMode
+    alterarMode() {
+      this.pomodoroMode = !this.pomodoroMode;
     },
     aumentarTempo() {
-      this.minutos++
+      this.minutos++;
     },
     diminuirTempo() {
-      this.minutos--
+      this.minutos--;
     },
-    iniciado(){
-      return this.segundos != '00' && ((this.pomodoroMode && this.minutos != 25) || (!this.pomodoroMode && this.minutos != 5))
+    iniciado() {
+      return (
+        this.segundos != "00" &&
+        ((this.pomodoroMode && this.minutos != 25) ||
+          (!this.pomodoroMode && this.minutos != 5))
+      );
     },
-    finalizado(){
-      return this.segundos == '00' && this.minutos == 0
+    finalizado() {
+      return this.segundos == "00" && this.minutos == 0;
     },
   },
-  watch:{
-    segundos:{
+  watch: {
+    segundos: {
       handler(value) {
-        let segundo = Number(value)
-        if(this.working){
+        let segundo = Number(value);
+        if (this.working) {
           setTimeout(() => {
-            if(segundo == 0){
-              if(this.minutos == 0){
-                this.working = false
-                const audio = new Audio(alarm)
-                audio.play()
-                if(this.pomodoroMode){
-                  this.contador++
+            if (segundo == 0) {
+              if (this.minutos == 0) {
+                this.working = false;
+                const audio = new Audio(alarm);
+                audio.play();
+                if (this.pomodoroMode) {
+                  this.contador++;
                 }
-                return
+                return;
               }
               this.minutos--;
-              this.segundos = '59'            
+              this.segundos = "59";
             }
-            segundo <= 10 ? this.segundos = `0${this.segundos - 1}` : this.segundos = `${this.segundos - 1}`;
+            segundo <= 10
+              ? (this.segundos = `0${this.segundos - 1}`)
+              : (this.segundos = `${this.segundos - 1}`);
           }, 10);
         }
-      }
+      },
     },
-    pomodoroMode : {
+    pomodoroMode: {
       handler(value) {
-        value ? this.minutos = 25 : this.minutos = 5
-        this.segundos = '00'
-      }
+        value ? (this.minutos = 25) : (this.minutos = 5);
+        this.segundos = "00";
+      },
     },
-    contador : {
+    contador: {
       handler(value) {
-        if (value % 4 == 0){
+        if (value % 4 == 0) {
           alert("Que tal descansar por 10 minutos ?");
         }
-      }
-    }
-  }
-}
+      },
+    },
+  },
+};
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
