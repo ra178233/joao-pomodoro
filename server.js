bb6a0c2eee4bf44ea68c33fe492b3a8ec48c7365
unknown <RA178233@UCDB.BR>
2021-11-17 08:57:26 -0400

    make it better

--- server.js
+++ server.js
@@ -5,7 +5,6 @@ var serveStatic = require('serve-static')
 var app = express()
 app.use(serveStatic(path.join(__dirname, 'dist')))
 
-var port = process.env.PORT || 3001
-app.listen(port, () => {
-    console.log(`APP listening at http://localhost:${port}`)
-});
+var port = process.env.PORT || 5000
+app.listen(port)
+console.log('server started ' + port)
