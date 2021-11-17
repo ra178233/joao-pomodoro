9845c33b1f68f337d0a913572abc621cdb867931
unknown <RA178233@UCDB.BR>
2021-11-17 11:56:39 -0400

    make it better

--- server.js
+++ server.js
@@ -3,8 +3,8 @@ var path = require('path')
 var serveStatic = require('serve-static')
 
 var app = express()
+exports.app = app
 app.use(serveStatic(path.join(__dirname, 'dist')))
 
 var port = process.env.PORT || 5000
-app.listen(port)
-console.log('server started ' + port)
+exports.port = port
