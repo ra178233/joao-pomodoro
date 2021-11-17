59e9cf95727ce1e9004eb34cea23d10cca71854e
unknown <RA178233@UCDB.BR>
2021-11-17 09:23:01 -0400

    make it better

--- server.js
+++ server.js
@@ -6,6 +6,5 @@ var app = express()
 app.use(serveStatic(path.join(__dirname, 'dist')))
 
 var port = process.env.PORT || 5000
-app.listen(port, () => {
-    console.log(`APP listening at http://localhost:${port}`)
-});
+app.listen(port)
+console.log('server started ' + port)
